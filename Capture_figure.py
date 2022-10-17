9b4913e70aee29583d1a6348bd369c6bed2a4ed6
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-10-17 12:57:49 +0900

    new

--- Capture_figure.py
+++ Capture_figure.py
@@ -3,6 +3,9 @@ import os
 import datetime
 
 
+WIDTH = 1280
+HEIGHT = 720
+
 now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 figurepath = os.getcwd() + '/camera_figure/'
@@ -10,8 +13,13 @@ if not os.path.exists(figurepath):
     os.mkdir(figurepath)
 
 capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH) ;
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT);
+
 ret, img = capture.read()
 
 cv2.imwrite(figurepath + now + '.png', img)
 
 capture.release()
+
+print('finish')
