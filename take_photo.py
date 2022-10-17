402ff0c3e360b1d4c7a86f8eed05edcf5cd31d27
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-10-17 16:15:57 +0900

    new

--- take_photo.py
+++ take_photo.py
@@ -4,16 +4,20 @@ from module import Capture as cap
 from module import light as LED
 
 
+# create folder
 figurepath = os.getcwd() + '/camera_figure/'
 cap.create_folder(figurepath)
 
-width = 2592
-height = 1944
-number = 3
+# setting parameter
+width = 2592         # photo width
+height = 1944        # photo height
+number = 3           # number of photos
 
+# take photos
 LED.setting()
 
 for n in range(0, number, 1):
+    print(str(n + 1) + '/' + number)
     LED.turn_on()
     time.sleep(1)
 
@@ -23,6 +27,5 @@ for n in range(0, number, 1):
 
     LED.turn_off()
     time.sleep(1)
-
 
 print('finish')
