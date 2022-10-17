991adc9997a6edcb48ff776b889f345bc7afd00c
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-10-17 14:15:19 +0900

    new

---
+++ module/Capture.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import datetime
+
+
+# create folder
+# figurepath = os.getcwd() + '/camera_figure/'
+def create_folder(figurepath):
+    if not os.path.exists(figurepath):
+        os.mkdir(figurepath)
+
+
+# resolution
+def caputre(width, height):
+    # take a photo
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width) ;
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height);
+
+    ret, img = capture.read()
+
+    capture.release()
+
+    return img
+
+
+def save_photo(figurepath, img):
+    # get now time
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # save a photo
+    cv2.imwrite(figurepath + now + '.png', img)
