3503b349eaee654d8f2d0d5ebcc89ee95d40cd1c
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-10-31 15:43:11 +0900

    new

--- GPIO_cleanup.py
+++ GPIO_cleanup.py
@@ -1,3 +1,9 @@
 from module import move_motor as stepmotor
 
-stepmotor.Cleanup()
+
+IN1 = 2              # blue to blue
+IN2 = 3              # pink to red
+IN3 = 4              # yellow to yellow
+IN4 = 17             # orange to black
+motor = stepmotor.C28BYJ48(IN1, IN2, IN3, IN4)
+motor.Cleanup()
